3943627136f1b5cdeb4c2c2de48e68001aeceadf
Omar Ahmed O <deeyndiid@gmail.com>
2023-10-31 20:44:28 +0300

    Add task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+def uppercase(str):
+    for c in str:
+        i = ord(c)
+        i = i - 32 if i in range(97, 123) else i
+        print("{}".format(chr(i)), end='')
+    print()
