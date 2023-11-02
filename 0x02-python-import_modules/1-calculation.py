bce90a559daa18e7309309e9fdbb75bf94f284cd
Omar Ahmed O <deeyndiid@gmail.com>
2023-11-02 13:13:21 +0300

    Fix requirement-fail

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,7 +6,8 @@ import calculator_1 as cal
 a = 10
 b = 5
 
-print("{} + {} = {}".format(a, b, cal.add(a, b)))
-print("{} - {} = {}".format(a, b, cal.sub(a, b)))
-print("{} * {} = {}".format(a, b, cal.mul(a, b)))
-print("{} / {} = {}".format(a, b, cal.div(a, b)))
+if __name__ == "__main__":
+    print("{} + {} = {}".format(a, b, cal.add(a, b)))
+    print("{} - {} = {}".format(a, b, cal.sub(a, b)))
+    print("{} * {} = {}".format(a, b, cal.mul(a, b)))
+    print("{} / {} = {}".format(a, b, cal.div(a, b)))
