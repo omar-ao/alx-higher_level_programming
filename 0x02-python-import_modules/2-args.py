ee6016704cc2b15d549cef09bde3835d366df997
Omar Ahmed O <deeyndiid@gmail.com>
2023-11-02 13:37:59 +0300

    Tabs tabs

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,13 +4,13 @@ import sys
 
 
 if __name__ == "__main__":
-	len_args = len(sys.argv) - 1
-	if len_args == 0:
-	    print("0 arguments.")
-	elif len_args == 1:
-		print("1 argument:")
-	else:
-	    print("{} arguments:".format(len_args))
+    len_args = len(sys.argv) - 1
+    if len_args == 0:
+        print("0 arguments.")
+    elif len_args == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(len_args))
 
-	for i in range(1, len_args + 1):
-		print("{}: {}".format(i, sys.argv[i]))
+    for i in range(1, len_args + 1):
+        print("{}: {}".format(i, sys.argv[i]))
