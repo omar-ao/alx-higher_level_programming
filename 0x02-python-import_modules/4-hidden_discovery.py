df6a0d8e3f5da17c8b19f247a83e4b0969258e76
root <deeyndiid@gmail.com>
2023-11-02 04:20:01 -0700

    Update task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 import hidden_4
 
-dir()
+if __name__ == "__main__":
+    names = dir(hidden_4)
+    for name in names:
+        if not name.startswith("__"):
+            print(name)
