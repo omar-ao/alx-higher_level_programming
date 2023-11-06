7ea470edaa0992b122b7c4faf4db8f1dd95bb6d7
Omar Ahmed O <deeyndiid@gmail.com>
2023-11-06 06:50:58 +0300

    Add task 14

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "Python.h"
+
+/**
+ * print_python_list_info - Prints some basic info about Python lists
+ *
+ * @p: Pointer to Python Object
+ */
+void print_python_list_info(PyObject *p)
+{
+	PyListObject *pp;
+	int i;
+
+	pp = (PyListObject *)p;
+	printf("[*] Size of the Python List = %ld\n", pp->ob_base.ob_size);
+	printf("[*] Allocated = %ld\n", pp->allocated);
+
+	for (i = 0; i < (int) pp->ob_base.ob_size; i++)
+	{
+		printf("Element %d: %s\n", i, pp->ob_item[i]->ob_type->tp_name);
+	}
+}
