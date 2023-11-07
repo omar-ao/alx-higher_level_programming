2e828cdbad4e2d734b2baad665c3815b12dfa586
Omar Ahmed O <deeyndiid@gmail.com>
2023-11-08 02:57:54 +0300

    Add task 2 second time

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,7 @@
+def replace_in_list(my_list, idx, element):
+    llen = len(my_list)
+    if idx < 0:
+        return my_list
+    elif idx >= llen:
+        return my_list
+    my_list[idx] = element
