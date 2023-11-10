90d9e40c2928ba87cfe77540ed8e56a38abdd5d1
Omar Ahmed O <deeyndiid@gmail.com>
2023-11-10 19:50:49 +0300

    Now handles non and empty string

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) is not str and len(roman_string) == 0:
+    if type(roman_string) is not str:
         return 0
+    elif not roman_string:
+        return 0
+
     numerals = {"I": 1,
                 "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     num = 0
