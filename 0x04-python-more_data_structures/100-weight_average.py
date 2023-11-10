26be17391d7667c17866be3b710b89a6af899e56
Omar Ahmed O <deeyndiid@gmail.com>
2023-11-10 18:18:42 +0300

    With pyhtho I feel I can do anything

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,3 +2,10 @@
 def weight_average(my_list=[]):
     if not my_list:
         return 0
+    weight = 0
+    count = 0
+    for elem in my_list:
+        a, b = elem
+        weight += a * b
+        count += b
+    return weight / count
