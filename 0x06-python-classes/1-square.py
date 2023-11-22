10f702c753b29816f82a94eb1f37db1e71272c77
Omar Ahmed O <deeyndiid@gmail.com>
2023-11-22 05:42:30 +0300

    Add task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """class Square that defines a square by: (based on 0-square.py)
+
+    Private instance attribute: size
+    """
+    def __init__(self, size):
+        """Creates a new instance of the class square
+        
+        arguments: size
+        """
+        self.__size = size
+    
