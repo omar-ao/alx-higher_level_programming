9b884c32f4b68b79d14d054cfbb38bc52a6f8fde
Omar Ahmed O <deeyndiid@gmail.com>
2023-11-28 03:22:21 +0300

    Update

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+""" This is the "0-rectangle" module.
+Which defines one class, Rectangle. For example
+>>> rec = Rectangle()
+>>> print(rec.__dict__)
+{}"""
+
 class Rectangle:
     """Empty class that defines a rectangle"""
     pass
