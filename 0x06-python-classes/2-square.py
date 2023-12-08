7c1d0df88601b5b9c6864cbb21f98397d85ad675
Omar Ahmed O <deeyndiid@gmail.com>
2023-12-08 08:44:17 +0300

    Add task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Class that defines square"""
+
+
+class Square():
+    """a class Sqaure that defines a square by:(based on 1-square.py.
+
+    Attributes:
+        size (int): size of the square
+    """
+
+    def __init__(self, size=0):
+        """Initialises an instance of the class Square
+
+        Args:
+            size (int): size of the instance
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Gets and returns the size of the instance.
+
+        Returns:
+            int: size of the square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of the square.
+
+        Args:
+            value (int): An integer value to set the size of the square
+        Raises:
+            TypeError: An error occurs when size is not integer
+            ValueError: An error occurs when size is less than zero
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
