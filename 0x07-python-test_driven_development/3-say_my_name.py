04e3058cb225017dfd0a581dbf74c8b563f5fb5e
Omar Ahmed O <deeyndiid@gmail.com>
2023-12-26 06:44:19 +0300

    Add say_my_name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This is the "3-say_my_name" module contains one function
+Usage example
+    >>> say_my_name("Ali", "Adan")
+    My name is Ali Adan
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Prints My name is <first name> <last name>
+    Usage example:
+    >>> say_my_name("Mohamed", "Hassan")
+    My name is Mohamed Hassan
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
