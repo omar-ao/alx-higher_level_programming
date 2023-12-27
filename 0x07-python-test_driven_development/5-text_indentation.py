97ca2c46fab537b958af074d98d965b753c4111c
Omar Ahmed O <deeyndiid@gmail.com>
2023-12-27 19:21:32 +0300

    Add task 4

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""This is the "5-text_indentation" module
+This module supplies one function, text_indentation().
+Usage example
+    >>> text_indentation("This: is/ TDD.")
+    This
+    
+    is
+
+    TDD
+"""
+
+
+def text_indentation(text):
+    """Prints a text with 2 new lines after each of
+    these characters: '.', '?' and ':'
+    Usage example
+    >>> text_indentation("This: is/ TDD.")
+    This
+    <BLANKLINE>
+    is
+    <BLANKLINE>
+    TDD
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    
+    for c in ".?:":
+        text = (c + "\n\n").join([line.strip(" ") for line in text.split(c)])
+
+    print(text, end="")
