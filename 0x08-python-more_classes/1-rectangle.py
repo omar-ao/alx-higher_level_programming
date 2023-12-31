46bba2f019278700cf0f337c566c27eb54d935ab
Omar Ahmed O <deeyndiid@gmail.com>
2023-12-31 22:21:07 +0300

    Add validation in the init method

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -18,11 +18,11 @@ class Rectangle:
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
         if width < 0:
-            raise ValueError("width must >= 0")
+            raise ValueError("width must be >= 0")
         self.width = width
 
         if not isinstance(height, int):
-            raise TypeError("height must an integer")
+            raise TypeError("height must be an integer")
         if height < 0:
             raise ValueError("height must be >= 0")
         self.height = height
