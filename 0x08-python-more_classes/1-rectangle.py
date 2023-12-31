dc1236f958d80e0f21ec4863374e52cb280b9c2e
Omar Ahmed O <deeyndiid@gmail.com>
2023-12-31 23:58:57 +0300

    Removed properties from the init method

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -15,33 +15,33 @@ class Rectangle:
             width (int): Width of the rectangle
             height (int): Height of the rectangle
         """
-        self.width = width
         self.height = height
+        self.width = width
 
-        @property
-        def width(self):
-            """Retries width"""
-            return self.__width
+    @property
+    def width(self):
+        """Retries width"""
+        return self.__width
 
-        @property
-        def height(self):
-            """Retrivies height"""
-            return self.__height
+    @property
+    def height(self):
+        """Retrivies height"""
+        return self.__height
 
-        @width.setter
-        def width(self, value):
-            """Sets width"""
-            if not isinstance(width, int):
-                raise TypeError("width must be an integer")
-            if width < 0:
-                raise ValueError("width must be >= 0")
-            self.__width = value
+    @width.setter
+    def width(self, value):
+        """Sets width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
-        @height.setter
-        def height(self, value):
-            """Sets height"""
-            if not isinstance(height, int):
-                raise TypeError("height must be an integer")
-            if height < 0:
-                raise ValueError("height must be >= 0")
-            self.__height = value
+    @height.setter
+    def height(self, value):
+        """Sets height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
