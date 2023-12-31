edce4b1b12c7da682102b9c2383a7c5032274a4a
Omar Ahmed O <deeyndiid@gmail.com>
2024-01-01 01:09:53 +0300

    Remove print new line

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -63,5 +63,6 @@ class Rectangle:
         for i in range(self.height):
             for j in range(self.width):
                 print("#", end="")
-            print()
+            if i != self.height - 1:
+                print()
         return ""
