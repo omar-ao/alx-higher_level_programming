43ffac9dfd5edf04353fcac0c6108d9158e6505f
root <deeyndiid@gmail.com>
2024-01-06 00:42:01 -0800

    Add task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This is ``1-my_list`` module
+This module defines My_list class that inherits from list
+Examples
+>>> my_list = My_list()
+>>> my_list.append(1)
+>>> my_list.append(2)
+>>> print(my_list)
+[1, 2]
+"""
+
+
+class My_list(list):
+    """Defines My_list which inherits from list"""
+
+    def print_sorted(self):
+        """prints sorted list in ascending order"""
+        return sorted(self)
