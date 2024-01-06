5bfd70b5323207914e8755bf08652b33efe5c2bc
root <deeyndiid@gmail.com>
2024-01-06 00:55:21 -0800

    Update task1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
 """This is ``1-my_list`` module
 This module defines My_list class that inherits from list
-Examples
->>> my_list = My_list()
->>> my_list.append(1)
->>> my_list.append(2)
->>> print(my_list)
-[1, 2]
 """
 
-
 class My_list(list):
-    """A class My_list defines my_list
-    My_list inherits from list class
+    """
+    A class My_list that defines my_list
     """
 
     def print_sorted(self):
