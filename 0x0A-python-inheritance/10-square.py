e7f367014be22d95a94d03af2214bb002857a993
Omar Ahmed O <deeyndiid@gmail.com>
2024-01-06 20:09:27 +0300

    Add task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This is the ``10-square``
+It defines a class Square that inherits from Rectangle(9-rectangle.py)
+"""
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """
+    Defines a class Square
+    """
+
+    def __init__(self, size):
+        """
+        Instantiates object from class Square
+        """
+        self.__size = size
+
+        super().integer_validator("size", self.__size)
+
+    def area(self):
+        """
+        Computes the area of square
+        """
+        return self.__size * self.__size
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
