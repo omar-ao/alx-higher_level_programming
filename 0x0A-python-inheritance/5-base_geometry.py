d65822315785483293b43fada7e381cfae0130a3
Omar Ahmed O <deeyndiid@gmail.com>
2024-01-06 17:54:52 +0300

    Add task 5

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This is ``5-base_geometry`` module
+It defines one class, BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    Defines class BaseGeometry
+    """
+    pass
