ae1ccdca907a468ac67ff1d969c78c86ea38b727
Omar Ahmed O <deeyndiid@gmail.com>
2024-01-07 18:45:11 +0300

    Add function documention

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,5 +5,8 @@ It contains one function, append_write
 
 
 def def append_write(filename="", text=""):
+    """Appends a string at the end of a text file
+    and returns the number of characters added
+    """
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(f)
