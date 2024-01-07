3982adae1e07d88bef556fff0cb0a9f495d2e677
Omar Ahmed O <deeyndiid@gmail.com>
2024-01-08 01:02:26 +0300

    Add task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""This is the ``10-student`` module
+It defines one class Student
+"""
+
+
+class Student:
+    """A class Student that defines a student
+    """
+    def __init__(self, first_name, last_name, age):
+        """Initializes the instance
+
+        Args:
+            first_name: Student first name
+            last_name: Student last name
+            age (int): Student age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrivies a dictionary representation of
+        Studnet instance
+
+        Only attribute names contained in this list is
+        retrieved. Otherwise all attributes are retrieved
+
+        Args:
+            attrs: A list of strings
+        """
+        obj_dict = self.__dict__
+        if attrs:
+            return {attr: obj_dict[attr] for attr in attrs if attr in obj_dict}
+        return obj_dict
