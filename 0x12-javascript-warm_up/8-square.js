4b1486b0c6ad17b43f43e98efc0bf8905fccf839
Omar Ahmed O <deeyndiid@gmail.com>
2024-02-12 21:26:55 +0300

    Update task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -6,9 +6,11 @@ if (!args[2]) {
   console.log('Missing size');
 }
 
+let line = '';
 for (let i = 0; i < Number.parseInt(args[2]); i++) {
+  line = '';
   for (let i = 0; i < Number.parseInt(args[2]); i++) {
-    process.stdout.write('X');
+    line += 'X';
   }
-  console.log();
+  console.log(line);
 }
