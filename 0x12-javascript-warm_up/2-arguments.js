cc0d8f2aba0dde1ddbb28edc9974002928c5a68f
Omar Ahmed O <deeyndiid@gmail.com>
2024-02-12 14:22:22 +0300

    Update task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 
-const { argv } = require('node:process');
+const args = process.argv;
 
-const len = argv.length;
-
-if (len === 0) {
+if ((args.length - 2) === 0) {
   console.log('No argument');
-} else if (len === 1) {
+} else if ((args.length - 2) === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
