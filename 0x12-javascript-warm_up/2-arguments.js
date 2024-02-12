dfb35e98c57f40117be3724c8ded2312a99e973b
Omar Ahmed O <deeyndiid@gmail.com>
2024-02-12 14:05:47 +0300

    Add

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+
+const len = argv.length - 2;
+
+if (len === 0) {
+  console.log('No argument');
+} else if (len === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
