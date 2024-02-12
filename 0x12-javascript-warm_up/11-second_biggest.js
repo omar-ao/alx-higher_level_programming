cb5512a9cfcebd871822b789a77cf897f6ad2152
Omar Ahmed O <deeyndiid@gmail.com>
2024-02-12 23:46:08 +0300

    Update task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,7 +6,7 @@ const numbers = args.slice(2);
 const len = numbers.length;
 
 if (len > 1) {
-  numbers.sort(function(a, b) {
+  numbers.sort(function (a, b) {
     return a - b;
   });
   console.log(numbers.slice(-2, -1)[0]);
