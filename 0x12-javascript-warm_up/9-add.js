9cd5de882b381a4ede59301bb0c631559f561b8a
Omar Ahmed O <deeyndiid@gmail.com>
2024-02-12 22:23:06 +0300

    Update task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,9 +2,9 @@
 
 const args = process.argv;
 
-function add(a, b) {
-	console.log(a + b);
-	return 0;
+function add (a, b) {
+  console.log(a + b);
+  return 0;
 }
 
 const first = Number.parseInt(args[2]);
