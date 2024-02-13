209583cb2dda9e7cf885c0e2ce2e1a07c6bb126b
Omar Ahmed O <deeyndiid@gmail.com>
2024-02-13 13:20:21 +0300

    Add task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if ((w >= 1 && h >= 1)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
+      }
+      console.log();
+    }
+  }
+};
