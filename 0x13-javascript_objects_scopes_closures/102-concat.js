d780e5cc175cfc2b76b124b1c78c35e8f3d92090
Omar Ahmed O <deeyndiid@gmail.com>
2024-02-14 21:55:33 +0300

    Update task 13

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,19 +1,15 @@
 #!/usr/bin/node
 
 const fs = require('node:fs/promises');
+const args = process.argv;
+const [fileA, fileB, fileC] = args.slice(2);
 
 async function concatFiles () {
-  try {
-    const fileA = await fs.readFile('fileA',
-      { encoding: 'utf8' });
-    const fileB = await fs.readFile('fileB',
-      { encoding: 'utf8' });
+  const contentFileA = fileA ? await fs.readFile(fileA) : '';
+  const contentFileB = fileB ? await fs.readFile(fileB) : '';
 
-    await fs.appendFile('fileC', fileA);
-    await fs.appendFile('fileC', fileB);
-  } catch (err) {
-    console.log(err);
-  }
+  await fs.appendFile(fileC, contentFileA);
+  await fs.appendFile(fileC, contentFileB);
 }
 
 concatFiles();
