e22017b5295e66602437447bf21018a8dff920c2
Omar Ahmed O <deeyndiid@gmail.com>
2024-02-17 11:31:26 +0300

    Add task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Script that takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where name matches
+the argument
+
+Usage:
+    ./2-my_filter_states.py <user> <password> <db name>
+"""
+
+
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    usr, pwd, db, arg = sys.argv[1:]
+    host, port = 'localhost', 3306
+
+    conn = MySQLdb.connect(host=host, port=port, user=usr, passwd=pwd, db=db)
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE BINARY name='{}' ORDER BY id"
+                .format(arg))
+    row = cur.fetchone()
+    print(row)
