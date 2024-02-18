ba1c551751673607b1ac66e6a2cbb1b44bbc0c19
Omar Ahmed O <deeyndiid@gmail.com>
2024-02-18 10:39:49 +0300

    Add task 8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+8-model_state_fetch_first.py
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, session
+from model_state import Base, State
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        sys.exit(1)
+
+    usr, pwd, db = sys.argv[1:]
+    url = "mysql://{}:{}@localhost:3306/{}".format(usr, pwd, db)
+
+    engine = create_engine(url)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).first()
+    if state:
+        print('{}: {}'.format(state.id, state.name))
+    else:
+        print()
