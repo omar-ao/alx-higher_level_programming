590086f531dd4932266591c77f310bd00f72fd3e
Omar Ahmed O <deeyndiid@gmail.com>
2024-02-18 09:45:06 +0300

    Update task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -10,6 +10,12 @@ Base = declarative_base()
 
 
 class State(Base):
+    """Defines class State that defines sql table states
+
+    attributes:
+        id (integer): talbe id
+        name (string): table name
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
