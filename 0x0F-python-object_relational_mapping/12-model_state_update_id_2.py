5bb714e3207183242a1f1bd3422801ac2f6a685d
Omar Ahmed O <deeyndiid@gmail.com>
2024-02-18 11:44:28 +0300

    Add task 12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+script that changes the name of a State object from the database hbtn_0e_6_usa
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, session
+from model_state import Base, State
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        sys.exit(1)
+
+    usr, pwd, db, = sys.argv[1:]
+    url = "mysql://{}:{}@localhost:3306/{}".format(usr, pwd, db)
+
+    engine = create_engine(url)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.id == 2).first()
+    new_name = 'New Mexico'
+    if state:
+        state.name = new_name
+        session.commit()
