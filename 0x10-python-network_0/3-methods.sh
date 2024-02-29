7a632f74aa19e68fa79b33098186fad1dc9130e3
Omar Ahmed O <deeyndiid@gmail.com>
2024-02-29 17:14:31 +0300

    Update 3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #This script takes in URL and displays all HTTP methods the server will accept
-curl -X OPTIONS "$1" 
+curl -I "$1" 2>/dev/null | awk '/Allow:/ {$1=""; print substr($0,2)}'
