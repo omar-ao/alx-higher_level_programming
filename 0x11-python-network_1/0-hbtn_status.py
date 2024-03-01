4b2874ac00b9775360c31fe0fae73a87090f3532
Omar Ahmed O <deeyndiid@gmail.com>
2024-03-01 15:17:07 +0300

    Add 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""script that fetches https://alx-intranet.hbtn.io/status"""
+
+import urllib.request
+
+
+url = 'https://alx-intranet.hbtn.io/status'
+req = urllib.request.Request(url)
+with urllib.request.urlopen(req) as response:
+    data = response.read()
+    print("\t- type: {}".format(type(data)))
+    print("\t- content: {}".format(data))
+    print("\t- utf content: {}".format(data.decode("utf-8")))
