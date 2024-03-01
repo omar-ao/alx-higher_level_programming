36361d4dc4822bd0e27a997c417e71beab6a58bc
Omar Ahmed O <deeyndiid@gmail.com>
2024-03-01 17:33:00 +0300

    Add 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""script that takes in a URL, sends a request to the URL and displays the
+value of the X-Request-Id variable found in the header of the response.
+"""
+
+import urllib.request
+import sys
+
+data = {}
+data['email'] = sys.argv[2]
+data = urllib.parse.urlencode(data).encode('utf-8')
+req = urllib.request.Request(sys.argv[1], data)
+with urllib.request.urlopen(req) as response:
+    print(response.read().decode('utf-i'))
