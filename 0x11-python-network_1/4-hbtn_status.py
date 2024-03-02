aede49fb8d0b6b0bdccd71c0a3e28867849016d3
Omar Ahmed O <deeyndiid@gmail.com>
2024-03-02 11:30:26 +0300

    Add 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+This is script that fetches https://alx-intranet.hbtn.io/status
+"""
+import requests
+
+
+if __name__ == '__main__':
+    url = 'https://alx-intranet.hbtn.io/status'
+    r = requests.get(url)
+    print('Body response:')
+    print('\t- type: {}'.format(type(r.text)))
+    print('\t- content: {}'.format(r.text))
