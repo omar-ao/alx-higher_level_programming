eca32a14d3415d98e38d48036c239ecd8a9173a0
Omar Ahmed O <deeyndiid@gmail.com>
2024-03-02 13:19:11 +0300

    Add 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""
+This script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    url = 'http://0.0.0.0:5000/search_user'
+    q = ""
+    if len(sys.argv) == 2:
+        q = sys.argv[1]
+
+    r = requests.post(url, data={'q': q})
+    try:
+        result = r.json()
+        if result:
+            print('[{}] {}'.format(result.get('id'), result.get('name')))
+        else:
+            print('No result')
+    except requests.exceptions.JSONDecodeError:
+        print('Not a valid JSON')
