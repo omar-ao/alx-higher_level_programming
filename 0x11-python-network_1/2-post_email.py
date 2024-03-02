2b1bd2632c10e8339058159c6b117bb35b6e7516
Omar Ahmed O <deeyndiid@gmail.com>
2024-03-02 08:26:13 +0300

    Update 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
-"""Script that takes in a URL, sends a request to the URL"""
+"""This script that takes in a URL and an email, sends a POST request
+to the passed URL with the email as a parameter, and displays
+the body of the response (decoded in utf-8)
+
+Usage:
+    ./2-post_email.py <url> <email>
+"""
 
 import urllib.request
 import sys
